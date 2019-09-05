6f38763be1043d248a97206af6ebd1d28acc3639
lowsa <lowsa@DESKTOP-N7AQ4C8>
2019-09-05 15:17:43 +0900

    수정

--- src/main/java/com/kh/finalProject/register/model/service/RegisterService.java
+++
@@ -1,9 +0,0 @@
-package com.kh.finalProject.register.model.service;
-
-import com.kh.finalProject.register.model.vo.Register;
-
-public interface RegisterService {
-
-	Register registerStudent(Register r);
-
-}
