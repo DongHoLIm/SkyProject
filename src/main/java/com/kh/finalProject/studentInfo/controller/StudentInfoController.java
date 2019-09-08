19ca0351d8b0c76af71a03499778ea019d90a4b3
지은 <@chaeje>
2019-09-08 12:00:06 +0900

    jieun

--- src/main/java/com/kh/finalProject/studentInfo/controller/StudentInfoController.java
+++ src/main/java/com/kh/finalProject/studentInfo/controller/StudentInfoController.java
@@ -1,12 +1,9 @@
 package com.kh.finalProject.studentInfo.controller;
 
-import java.util.HashMap;
-
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -23,15 +20,21 @@ public class StudentInfoController {
 	@Autowired
 	private StudentInfoService ss;
 	
-	//학적조회 메인
+	//학생_학적조회 메인
 	@RequestMapping(value="studentInfo.st")
-	public String infoMain(@ModelAttribute("loginUser") Member loginUser) {
+	public String infoMain(HttpServletRequest request, @ModelAttribute("loginUser") Member loginUser) {
 		
 		String userId = loginUser.getMemberId();
 		System.out.println(userId);
 		
 		StudentInfo basicInfo = ss.basicInfo(userId);
 		StudentInfo stuInfo = ss.stuInfo(userId);
+		
+		System.out.println(basicInfo);
+		System.out.println(stuInfo);
+		
+		request.setAttribute("basicInfo",basicInfo);
+		request.setAttribute("stuInfo",stuInfo);
 		
 		
 		return "student/info/schoolRegister";
