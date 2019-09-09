77ec1b1e602a5667970c02552fbf4f7bc9b6bc2d
82109 <82109@LAPTOP-IAQN4101>
2019-09-09 14:48:08 +0900

    20190909(1)..!

---
+++ src/main/java/com/kh/finalProject/dormitory/model/service/DormitoryService.java
@@ -0,0 +1,12 @@
+package com.kh.finalProject.dormitory.model.service;
+
+import java.util.ArrayList;
+
+import com.kh.finalProject.dormitory.model.exception.DormitoryException;
+import com.kh.finalProject.dormitory.model.vo.Dormitory;
+
+public interface DormitoryService {
+
+	ArrayList<Dormitory> userDormitory(String studentNo) throws DormitoryException;
+
+}
