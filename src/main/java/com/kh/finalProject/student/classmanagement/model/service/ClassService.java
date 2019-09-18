ec6ecfcc7b441c51332de513c3d65ad78505aeb3
anfeo <anfeo@DESKTOP-HB9OGV7>
2019-09-18 14:31:45 +0900

    아.....

--- src/main/java/com/kh/finalProject/student/classmanagement/model/service/ClassService.java
+++ src/main/java/com/kh/finalProject/student/classmanagement/model/service/ClassService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import com.kh.finalProject.employee.classManagement.model.vo.OpenSubject;
 import com.kh.finalProject.student.classmanagement.model.vo.Sdepartment;
+import com.kh.finalProject.student.classmanagement.model.vo.SubjectApply;
 
 public interface ClassService {
 
@@ -20,5 +21,9 @@ public interface ClassService {
 	ArrayList<OpenSubject> selectProfessor(String professor);
 
 	ArrayList<OpenSubject> selectProGwamok(OpenSubject os);
+
+	void insertCourseApply(String[] subCode, SubjectApply sa);
+
+	ArrayList<SubjectApply> selectMySugang(SubjectApply sa);
 
 }
