e1290ee2fae262689aad4bcea74d00d103a13fa8
user1 <user1@DESKTOP-FLCC6CM>
2019-09-20 15:06:41 +0900

    dggdgd

---
+++ src/main/java/com/kh/finalProject/employee/classManagement/model/service/LessonPlanServiceImple.java
@@ -0,0 +1,24 @@
+package com.kh.finalProject.employee.classManagement.model.service;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.finalProject.employee.classManagement.model.dao.LessonPlanDao;
+import com.kh.finalProject.employee.classManagement.model.vo.LessonPlan;
+@Service
+public class LessonPlanServiceImple  implements LessonPlanService{
+
+	@Autowired
+	LessonPlanDao lpd;
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+
+	@Override
+	public void insertLessonPlan(LessonPlan lp) {
+		lpd.insertLessonPlan(sqlSession, lp);	
+	}
+	
+
+}
