1157f6d7cb5f28dac3fba1b711253561fa006cfa
lowsa <lowsa@DESKTOP-N7AQ4C8>
2019-09-23 13:34:45 +0900

    수정

--- src/main/java/com/kh/finalProject/studentInfo/controller/StudentInfoController.java
+++ src/main/java/com/kh/finalProject/studentInfo/controller/StudentInfoController.java
@@ -6,7 +6,6 @@ import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
