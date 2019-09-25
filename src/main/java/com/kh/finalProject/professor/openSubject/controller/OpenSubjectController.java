30cc47ca3a46cf93c4b99dfc355222497f1a29cd
user1 <user1@DESKTOP-FLCC6CM>
2019-09-25 14:03:30 +0900

    dd

--- src/main/java/com/kh/finalProject/professor/openSubject/controller/OpenSubjectController.java
+++ src/main/java/com/kh/finalProject/professor/openSubject/controller/OpenSubjectController.java
@@ -2,25 +2,29 @@ package com.kh.finalProject.professor.openSubject.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.kh.finalProject.employee.classManagement.model.service.LessonPlanService;
+import com.kh.finalProject.employee.classManagement.model.vo.LessonPlan;
 import com.kh.finalProject.member.model.vo.Member;
 import com.kh.finalProject.professor.openSubject.model.service.OpenSubjectService;
 import com.kh.finalProject.professor.openSubject.model.vo.OpenSubject;
 
 @Controller
 public class OpenSubjectController {
-	
+	@Autowired
+	LessonPlanService lps;
 	@Autowired
 	OpenSubjectService openSubjectService;
+	
+	
+	
 	
 	@RequestMapping("opensubject.pro")
 	public ModelAndView OpenSubject (ModelAndView mav) {
@@ -64,7 +68,16 @@ public class OpenSubjectController {
 			
 		}
 	
+	@RequestMapping("lessonPlan.pro")
+	public ModelAndView LessonPlan (LessonPlan lp, ModelAndView mav) {
+		lps.insertLessonPlan(lp);
+		List<OpenSubject> openSubjectList = openSubjectService.selectOpenSubjectList();
+		System.out.println("openSubjectList : " + openSubjectList);
+		mav.addObject("openSubjectList", openSubjectList);
+		mav.setViewName("professor/class/syllabus");
+		return mav;
 	
+	}
 	
 
 }
